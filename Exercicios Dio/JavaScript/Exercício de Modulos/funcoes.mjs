c7dc92916c21a9e91bd5d22051e0313c50e6f5ad
Erick Rosa <erickpmotta@gmail.com>
2021-10-15 00:44:39 -0300

    new exercise

---
+++ Exercicios Dio/JavaScript/Exercício de Modulos/funcoes.mjs
@@ -0,0 +1,13 @@
+function mostraIdade(nome, idade) {
+  return `A idade de ${nome} é ${idade}`;
+}
+
+function mostraCidade(nome, cidade) {
+  return `A Cidade de ${nome} é ${cidade}`;
+}
+
+function mostraHobby(nome, hobby) {
+  return `O Hobby de ${nome} é ${hobby}`;
+}
+
+export { mostraCidade, mostraHobby, mostraIdade };
---
+++ Exercicios Dio/JavaScript/Exercício de Modulos/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Erick", 26));
+console.log(mostraCidade("Erick", "Rio de Janeiro"));
+console.log(mostraHobby("Erick", "Filmes"));
